952b947f68cf9f4c51c1650cbb8c62549e1932bc
Nawel Hammouda <nawelks.info@gmail.com>
2026-09-05 09:08:38 +0100

    Fix Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,24 @@
-import { defineConfig, PluginOption } from "vite";
-import { enterDevPlugin, enterProdPlugin } from 'vite-plugin-enter-dev';
+import { defineConfig } from "vite";
 import path from "path";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const plugins = [
-    ...enterProdPlugin(),
-  ];
-  if (mode === 'development') {
-    plugins.push(...enterDevPlugin());
-  }
-  return {
-    server: {
-      host: "::",
-      port: 8080,
+export default defineConfig({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+
+  plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: plugins.filter(Boolean) as PluginOption[],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-    base: '/',
-    build: {
-      outDir: 'dist',
-    }
-  };
+  },
+
+  base: "/",
+
+  build: {
+    outDir: "dist",
+  },
 });
